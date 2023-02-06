47a143f6b74d1c6c68b3163a53b3ce6bc1d8c091
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-06 21:54:50 +0900

    ルーティングをいじった

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,8 @@
 
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\indexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +16,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //
-Route::get('/', function () {
-    return view('welcome');
-})->where('any','.*');
-//php artisan ui bootstrap --auth で生成されるルーティング設定
+//Route::get('/', function () {
+//    return view('index');
+//});
+
+
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [IndexController::class, 'index'])->name('index');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+
