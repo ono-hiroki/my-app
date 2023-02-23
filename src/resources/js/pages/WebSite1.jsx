e02b5ac30ba2b79f55ccf0a4a3a654fb9b04a524
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-24 08:32:28 +0900

    react-katexのuninstall

--- src/resources/js/pages/WebSite1.jsx
+++ src/resources/js/pages/WebSite1.jsx
@@ -1,20 +1,20 @@
 import {createRoot} from "react-dom/client";
 import React from "react";
 import 'katex/dist/katex.min.css';
-import {InlineMath, BlockMath} from 'react-katex';
+// import {InlineMath, BlockMath} from 'react-katex';
 
-import 'katex/dist/katex.min.css'
+// import 'katex/dist/katex.min.css'
 
 
 const App = () => {
     return (
         <div>
-            <InlineMath>\int_0^\infty x^2 dx</InlineMath>
-            <BlockMath
-                math={'\\int_0^\\infty x^2 dx'}
-                errorColor={'#cc0000'}
-            />
-            <BlockMath math={`\\frac{\\text{m}} {\\text{s}^2}`}/>
+            {/*<InlineMath>\int_0^\infty x^2 dx</InlineMath>*/}
+            {/*<BlockMath*/}
+            {/*    math={'\\int_0^\\infty x^2 dx'}*/}
+            {/*    errorColor={'#cc0000'}*/}
+            {/*/>*/}
+            {/*<BlockMath math={`\\frac{\\text{m}} {\\text{s}^2}`}/>*/}
             <h1>Laravel aa999</h1>
         </div>
     );
