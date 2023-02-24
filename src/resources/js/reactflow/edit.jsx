15596f8845315f1274bc8f4185590bc26e504922
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-24 20:30:19 +0900

    reactflowの導入

--- src/resources/js/reactflow/edit.jsx
+++ src/resources/js/reactflow/edit.jsx
@@ -1,10 +1,34 @@
 import {createRoot} from "react-dom/client";
 import React from "react";
+import ReactFlow, {Controls, Background} from 'reactflow';
+import 'reactflow/dist/style.css';
+import {useGetWindowSize} from "../hooks/useGetWindowSize";
+
+const edges = [{id: '1-2', source: '1', target: '2'}];
+
+const nodes = [
+    {
+        id: '1',
+        data: {label: 'Hello'},
+        position: {x: 0, y: 0},
+        type: 'input',
+    },
+    {
+        id: '2',
+        data: {label: 'World'},
+        position: {x: 100, y: 100},
+    },
+];
 
 const App = () => {
+    const {width, height} = useGetWindowSize();
+
     return (
-        <div>
-            <h1>Laravel 9</h1>
+        <div style={{width: width, height: height}}>
+            <ReactFlow nodes={nodes} edges={edges}>
+                <Background/>
+                <Controls/>
+            </ReactFlow>
         </div>
     );
 }
@@ -12,5 +36,5 @@ const App = () => {
 if (document.getElementById('root-container')) {
     const root = createRoot(document.getElementById('root-container'));
 
-    root.render(<App />);
+    root.render(<App/>);
 }
