9fc9d719b898b1d77410c25fc7e2638bfbc1357a
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-25 21:56:06 +0900

    nodeとedgesの更新系のロジックを移動

---
+++ src/resources/js/components/reactflow/store.js
@@ -0,0 +1,70 @@
+import {
+    applyNodeChanges,
+    applyEdgeChanges,
+} from 'reactflow';
+import {create} from 'zustand';
+
+let id = 3;
+const getId = () => `edge_${id++}`;
+
+const useStore = create((set, get) => ({
+    nodes: [
+        {id: 'node-1', type: 'textUpdater', position: {x: 0, y: 0}, data: {value: 123}},
+        {id: 'node-2', type: 'textUpdater', position: {x: 200, y: 0}, data: {value: 456}},
+        {id: 'node-3', type: 'textUpdater', position: {x: 100, y: 100}, data: {value: 789}},
+    ],
+    edges: [
+    ],
+    // ノードを動かすため
+    onNodesChange: (changes) => {
+        set({
+            nodes: applyNodeChanges(changes, get().nodes),
+        });
+    },
+    // エッジをつなげたり切ったりするため
+    onEdgesChange: (changes) => {
+        set({
+            edges: applyEdgeChanges(changes, get().edges),
+        });
+    },
+    // const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+    onConnect: (params) => {
+        const id = getId();
+        set({
+            edges: [...get().edges, {id, ...params}],
+        });
+
+
+    },
+
+    onDrop: (event, reactFlowInstance, reactFlowWrapper) => {
+        event.preventDefault(); // preventDefaultは、デフォルトのイベントをキャンセルするためのメソッドです。
+        const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
+        const type = event.dataTransfer.getData('application/reactflow');
+        // check if the dropped element is valid
+        if (typeof type === 'undefined' || !type) {
+            return;
+        }
+        // reactFlowBounds.leftは、要素の左端からの距離を示す数値です。
+        const position = reactFlowInstance.project({
+            x: event.clientX - reactFlowBounds.left,
+            y: event.clientY - reactFlowBounds.top,
+        });
+        const newNode = {
+            id: getId(),
+            type,
+            position,
+            data: {label: `${type} node`},
+        };
+
+        set((state) => ({nodes: [...state.nodes, newNode]}));
+    },
+
+    onDragOver: (event) => {
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'move';
+    },
+}));
+
+export default useStore;
--- src/resources/js/reactflow/edit.jsx
+++ src/resources/js/reactflow/edit.jsx
@@ -2,93 +2,52 @@ import {createRoot} from "react-dom/client";
 import React, {useCallback, useRef, useState} from "react";
 import ReactFlow, {
     ReactFlowProvider,
-    applyNodeChanges,
-    applyEdgeChanges,
-    addEdge,
-    useNodesState,
-    useEdgesState
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import {useGetWindowSize} from "../hooks/useGetWindowSize";
 import TextUpdaterNode from "../components/reactflow/custom-nodes/TextUpdaterNode";
 import Sidebar from "../components/reactflow/Sidebar";
+import useStore from "../components/reactflow/store";
+import {shallow} from "zustand/shallow";
 
-const initialNodes = [
-    {id: 'node-1', type: 'textUpdater', position: {x: 0, y: 0}, data: {value: 123}},
-    {id: 'node-2', type: 'textUpdater', position: {x: 200, y: 0}, data: {value: 456}},
-    {id: 'node-3', type: 'textUpdater', position: {x: 100, y: 100}, data: {value: 789}},
-];
 const nodeTypes = {textUpdater: TextUpdaterNode};
 
 const rfStyle = {
     backgroundColor: '#B8CEFF',
 };
 
-let id = 0;
-const getId = () => `dndnode_${id++}`;
+const selector = (state) => ({
+    nodes: state.nodes,
+    edges: state.edges,
+    onNodesChange: state.onNodesChange,
+    onEdgesChange: state.onEdgesChange,
+    addChildNode: state.addChildNode,
+    onConnect: state.onConnect,
+    onDragOver: state.onDragOver,
+    onDrop: state.onDrop,
+});
 
 const App = () => {
-    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-    const {width, height} = useGetWindowSize();
-    const onConnect = useCallback((connection) => setEdges((eds) => addEdge(connection, eds)), [setEdges]);
-    const reactFlowWrapper = useRef(null);
+    const { nodes, edges, onNodesChange, onEdgesChange, onConnect, onDragOver, onDrop } = useStore(selector, shallow);
     const [reactFlowInstance, setReactFlowInstance] = useState(null);
-    const onDragOver = useCallback((event) => {
-        event.preventDefault();
-        event.dataTransfer.dropEffect = 'move';
-    }, []);
-    const onDrop = useCallback(
-        (event) => {
-            event.preventDefault(); // preventDefaultは、デフォルトのイベントをキャンセルするためのメソッドです。
-
-            // @ts-ignore
-            const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
-            // getBoundingClientRect()は、要素の大きさと位置を示すDOMRectオブジェクトを返します。
-
-            const type = event.dataTransfer.getData('application/reactflow');
-
-            // check if the dropped element is valid
-            if (typeof type === 'undefined' || !type) {
-                return;
-            }
-
-            console.log(event.clientX, '-', reactFlowBounds.left, 'event.clientX - reactFlowBounds.left')
-            console.log(reactFlowBounds)
-            // reactFlowBounds.leftは、要素の左端からの距離を示す数値です。
-            // @ts-ignore
-            const position = reactFlowInstance.project({
-                x: event.clientX - reactFlowBounds.left,
-                y: event.clientY - reactFlowBounds.top,
-            });
-            const newNode = {
-                id: getId(),
-                type,
-                position,
-                data: {label: `${type} node`},
-            };
-
-            setNodes((nds) => nds.concat(newNode));
-        },
-        [reactFlowInstance]
-    );
-
+    const {width, height} = useGetWindowSize();
+    const reactFlowWrapper = useRef(null);
 
     return (
-        <div className="dndflow" style={{width: width, height: height}}>
+        <div className="dndflow">
             <ReactFlowProvider>
-                <div className="reactflow-wrapper" ref={reactFlowWrapper}>
+                <div className="reactflow-wrapper" ref={reactFlowWrapper}  style={{width: width, height: height}}>
                     <ReactFlow
                         nodes={nodes}
                         edges={edges}
                         onNodesChange={onNodesChange}
                         onEdgesChange={onEdgesChange}
-                        onConnect={onConnect}
+                        onConnect={(params) => onConnect(params)}
                         nodeTypes={nodeTypes}
                         fitView
                         style={rfStyle}
                         onInit={setReactFlowInstance}
-                        onDrop={onDrop}
+                        onDrop={(event) => onDrop(event, reactFlowInstance, reactFlowWrapper)}
                         onDragOver={onDragOver}
                     />
                 </div>
